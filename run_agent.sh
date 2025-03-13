73c1197c84238dc9587aa7209d4d3ab3913c05cd
Sfgangloff <37847888+Sfgangloff@users.noreply.github.com>
2025-03-13 09:25:03 +0100

    abstracting GNOSIS_RPC_0

--- run_agent.sh
+++ run_agent.sh
@@ -40,5 +40,8 @@ echo "Starting Tendermint..."
 tendermint node --proxy_app=tcp://127.0.0.1:26658 --rpc.laddr=tcp://127.0.0.1:26657 --p2p.laddr=tcp://0.0.0.0:26656 --p2p.seeds= --consensus.create_empty_blocks=true > /dev/null 2>&1 &
 tm_subprocess_pid=$!
 
+# Copy env variables
+CONNECTION_LEDGER_CONFIG_LEDGER_APIS_GNOSIS_ADDRESS="$CONNECTION_LEDGER_CONFIG_LEDGER_APIS_ETHEREUM_ADDRESS"
+
 # Run the agent
 aea -s run --env .agentenv
--- run_service.sh
+++ run_service.sh
@@ -5,6 +5,11 @@ rm -r mech
 # Load env vars
 set -o allexport; source .1env; set +o allexport
 
+GNOSIS_RPC_0="$ETHEREUM_LEDGER_RPC_0"
+GNOSIS_RPC_1="$ETHEREUM_LEDGER_RPC_1"
+GNOSIS_RPC_2="$ETHEREUM_LEDGER_RPC_2"
+GNOSIS_RPC_3="$ETHEREUM_LEDGER_RPC_3"
+
 # Remove previous builds
 # if [ -d "mech" ]; then
 #     echo $PASSWORD | sudo -S sudo rm -Rf mech;

5276a86c0e62fb80227980e832d5917315b5ad11
Sfgangloff <37847888+Sfgangloff@users.noreply.github.com>
2025-03-13 18:06:27 +0100

    Update run_agent.sh
    Co-authored-by: Adamantios Zaras <adamantioszaras@gmail.com>

--- run_agent.sh
+++ run_agent.sh
@@ -76,6 +76,5 @@ echo "Starting Tendermint..."
 tendermint node --proxy_app=tcp://127.0.0.1:26658 --rpc.laddr=tcp://127.0.0.1:26657 --p2p.laddr=tcp://0.0.0.0:26656 --p2p.seeds= --consensus.create_empty_blocks=true > /dev/null 2>&1 &
 tm_subprocess_pid=$!
 
-
 # Run the agent
 aea -s run --env .1env
